a805f3f28af2ac47b13aab63a4282293e9ec000b
robpap96 <robertopapagni1996@outlook.it>
2022-10-26 18:32:25 +0200

    aggiunti i chevron

--- js/main.js
+++ js/main.js
@@ -9,6 +9,9 @@ const images = [
     '<img src="img/05.jpg">'
 ];
 
+const chevronUp = '<i class="fa-solid fa-chevron-up"></i>';
+const chevronDown = '<i class="fa-solid fa-chevron-down"></i>';
+
 //creo le variabili che mi servono
 let active = 0;
 const container = document.querySelector('.container');
@@ -25,11 +28,16 @@ const items = document.querySelector('.items');
 const prev = document.createElement('div');
 prev.classList.add('prev');
 items.append(prev);
+document.querySelector('.prev').innerHTML = chevronUp;
+
 
 //creo ed inserisco il div next dentro items
 const next = document.createElement('div');
 next.classList.add('next');
 items.append(next);
+document.querySelector('.next').innerHTML = chevronDown;
+
+
 
 //creo il ciclo che inserisce i div nel container con le rispettive immagini
 for(let i=0; i<images.length; i++) {
@@ -37,7 +45,6 @@ for(let i=0; i<images.length; i++) {
     item.classList.add('item');
     item.id = i;
     items.append(item);
-
     document.getElementById(i).innerHTML = images[i];
 }
 
